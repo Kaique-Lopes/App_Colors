36c5a1bafc0c408bfde36d97afd46077ef4394a3
Kaique <kaique.lopes3271@gmail.com>
2021-09-21 17:54:21 -0300

    Functions

--- ViewController.swift
+++ ViewController.swift
@@ -8,10 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var vi: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var btnYellow: UIButton!
+    @IBOutlet weak var btnRed: UIButton!
+    @IBOutlet weak var btnBlack: UIButton!
+    
     override func viewDidLoad() {
+        vi.backgroundColor = .systemGray
+        view.backgroundColor = .systemGray
+        btnYellow.tintColor = .yellow
+        btnRed.tintColor = .red
+        btnBlack.tintColor = .black
+
         super.viewDidLoad()
     }
-
+    
+    @IBAction func btColorYellow(_ sender: UIButton) {        vi.backgroundColor = .yellow
+        label.text = "Amarelo"
+        label.textColor = .yellow
+    }
+    
+    @IBAction func btColorRed(_ sender: UIButton) {
+        vi.backgroundColor = .red
+        label.text = "Vermelho"
+        label.textColor = .red
+    }
+    
+    @IBAction func btColorBlack(_ sender: UIButton) {
+        vi.backgroundColor = .black
+        label.text = "Preto"
+        label.textColor = .black
+    }
+    
+    @IBAction func resetColors(_ sender: Any) {
+        vi.backgroundColor = .systemGray
+    }
+    
 
 }
